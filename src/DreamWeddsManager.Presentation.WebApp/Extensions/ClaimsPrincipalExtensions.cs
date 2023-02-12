bbb1cfc30cb2a27dc0cdfaf0fd33daad8d208b03
Neer S <neer.s@outlook.com>
2023-02-12 15:11:34 +0530

    WebApp Changes only

--- src/DreamWeddsManager.Presentation.WebApp/Extensions/ClaimsPrincipalExtensions.cs
+++ src/DreamWeddsManager.Presentation.WebApp/Extensions/ClaimsPrincipalExtensions.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using DreamWeddsManager.Shared.Constants.Permission;
 using System.Security.Claims;
 
 namespace SmartAdmin.WebUI.Extensions
