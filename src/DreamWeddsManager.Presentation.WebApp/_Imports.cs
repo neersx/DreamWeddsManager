849a4a56954548d1312a107bc64903fadf8f730a
Neer S <neer.s@outlook.com>
2023-02-14 23:48:56 +0530

    Removed Web Project

--- src/DreamWeddsManager.Presentation.WebApp/_Imports.cs
+++
@@ -1,8 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-global using MediatR;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.AspNetCore.Mvc.RazorPages;
-global using Microsoft.Extensions.Localization;
-global using Microsoft.AspNetCore.Authorization;
